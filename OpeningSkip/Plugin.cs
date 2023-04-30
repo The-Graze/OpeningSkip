3f590d727f59199da1ace272635a370536a3bd99
Graze <graze.the.sergal@gmail.com>
2023-04-30 11:28:37 +0100

    Add project files.

---
+++ OpeningSkip/Plugin.cs
@@ -0,0 +1,36 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+
+namespace OpeningSkip
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        void Awake()
+        {
+            SceneHelper.LoadScene("Main Menu", true);
+        }
+        void OnEnable()
+        {
+            SceneHelper.LoadScene("Main Menu", true);
+        }
+        void Update()
+        {
+            SceneHelper.LoadScene("Main Menu", true);
+            FindObjectOfType<InitGame>().enabled = false;
+            GameObject.Find("Init").SetActive(false);
+            GameObject.Find("Canvas").SetActiveRecursively(false);
+            Camera.main.gameObject.SetActive(false);
+            SceneHelper.LoadScene("Main Menu", true);
+            if(SceneHelper.CurrentScene == "Main Menu")
+            {
+                Destroy(this);
+            }
+        }
+        void OnDisable()
+        {
+            Destroy(this);
+        }
+    }
+}
---
+++ OpeningSkip/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace OpeningSkip
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.graze.gorillatag.openingskip";
+        public const string Name = "OpeningSkip";
+        public const string Version = "1.0.0";
+    }
+}
